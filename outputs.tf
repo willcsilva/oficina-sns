239aa8149afc66ef2730997e9ea378c258bbc70b
willcsilva <willcsilva15@gmail.com>
2026-02-07 17:14:12 -0300

    output SNS

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,3 @@ output "sns_topic_arn" {
   description = "O ARN completo do tópico SNS para o Kubernetes"
 }
 
-output "sns_topic_arn" {
-  description = "ARN do tópico SNS para notificações"
-  value       = aws_sns_topic.oficina_notificacoes.arn # Verifique se o nome do recurso é este mesmo
-}
